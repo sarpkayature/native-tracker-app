2841d85ca412d49e678e0e4882b8bb8a17c11225
Sarpkaya Ture <sarpkayature@gmail.com>
2022-03-23 23:31:35 +0300

    Add user password hash & compare password

--- api/src/modules/auth/models/User.js
+++ api/src/modules/auth/models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const bcrypt = require("bcrypt");
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -13,4 +13,38 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-mongoose.model("User", userSchema);
+userSchema.pre("save", function (next) {
+  const user = this;
+  if (!user.isModified("password")) {
+    return next();
+  }
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function (candidatePassword) {
+  const user = this;
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(candidatePassword, user.password, () => {
+      if (err) {
+        return reject(err);
+      }
+      if (!isMatch) {
+        return reject(false);
+      }
+      resolve(true);
+    });
+  });
+};
+
+module.exports = userSchema;
