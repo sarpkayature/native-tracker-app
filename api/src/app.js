b77a0371ef5ec7fd3de2a494a1fd739d1be97125
Sarpkaya Ture <sarpkayature@gmail.com>
2022-03-24 00:53:34 +0300

    Add global auth middleware

--- api/src/app.js
+++ api/src/app.js
@@ -2,21 +2,24 @@ const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 
+const requireAuth = require("./modules/middlewares/requireAuth");
+
 //*Routes imports
 const publicRoutes = require("./modules/defaults/routes/public-routes-index");
 const protectedRoutes = require("./modules/defaults/routes/protected-routes-index");
 
-//*App Middlewares
+//*Express instance
 const app = express();
+
+//*App Middlewares
 app.use(bodyParser.json());
 
 //*Public Routes
 publicRoutes.forEach((r) => app.use(r));
 
 //*Protected Routes
-protectedRoutes.forEach((r) => app.use(r));
+protectedRoutes.forEach((r) => app.use(r, requireAuth));
 
-//*MIDDLEWARES
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
