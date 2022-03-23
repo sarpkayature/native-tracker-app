c48d3c7f2dcff0bf9342a552f288bba231b68b75
Sarpkaya Ture <sarpkayature@gmail.com>
2022-03-23 23:32:13 +0300

    Add auth route

--- api/src/modules/auth/routes/authRoutes.js
+++ api/src/modules/auth/routes/authRoutes.js
@@ -1,14 +1,38 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const User = mongoose.model("User");
+const jwt = require("jsonwebtoken");
+const { user } = require("../../defaults/models/models");
 
 const router = express.Router();
 
 router.post("/signup", async (req, res) => {
   const { email, password } = req.body;
-  const user = new User({ email, password });
-  await user.save();
-  res.send("post request");
+
+  try {
+    const User = new user({ email, password });
+    await User.save();
+
+    const token = jwt.sign({ userId: User._id }, "MY_SECRET_TOKEN");
+    res.send({ token });
+  } catch (err) {
+    res.status(422).send(err.message);
+  }
+});
+
+router.post("/signup", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(422).send({ error: "Invalid password or email" });
+  }
+  const user = await user.findOne({ email });
+
+  try {
+    await user.comparePassword(password);
+    const token = jwt.sign({ userId: user._id }, "MY_SECRET_TOKEN");
+
+    res.send({ token });
+  } catch (err) {
+    return res.status(422).send({ error: "Invalid password or email" });
+  }
 });
 
 module.exports = router;
