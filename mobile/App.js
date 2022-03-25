b04a8a71bc1df9dd01089c903cfbfbe0add46afe
Sarpkaya Ture <sarpkayature@gmail.com>
2022-03-26 00:45:29 +0300

    Remove Styles

--- mobile/App.js
+++ mobile/App.js
@@ -1,16 +1,6 @@
-import { StyleSheet } from 'react-native'
 import AppNavigator from './src/routes/AppNavigator'
 const App = () => {
   return <AppNavigator />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
 
 export default App
